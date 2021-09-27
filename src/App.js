8e3b806681ab99ae437da94d75c0e76dff8b9c54
Sidharth Mishra <sidharth1604@gmail.com>
2021-09-27 19:09:00 +0530

    Files Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import "./App.css";
 import React, { Component } from 'react'
 import MediaNavbar from './Component/MediaNavbar'
+import News from './Component/News'
 
 export class App extends Component {
   render() {
     return (
       <>
         <MediaNavbar/>
-        
+        <News/>
       </>
     )
   }
--- src/Component/News.js
+++ src/Component/News.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
 export class News extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <>
+                <div className="container my-4">
+                    <h2 className="text-center">Today's Top Headlines</h2>
+                    <div className="row mt-5">
+                        <div className="col-md-3">
+                            <NewsItem title="News Today" description="Aaj ki taza khabar" imgUrl="https://static.toiimg.com/thumb/msid-86552833,width-1070,height-580,imgsize-34674,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"/>
+                        </div>
+                        <div className="col-md-3">
+                            <NewsItem title="News Today" description="Aaj ki taza khabar"/>
+                        </div>
+                        <div className="col-md-3">
+                            <NewsItem title="News Today" description="Aaj ki taza khabar"/>
+                        </div>
+                        <div className="col-md-3">
+                            <NewsItem title="News Today" description="Aaj ki taza khabar"/>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-md-3">
+                            <NewsItem title="News Today" description="Aaj ki taza khabar"/>
+                        </div>
+                        <div className="col-md-3">
+                            <NewsItem title="News Today" description="Aaj ki taza khabar"/>
+                        </div>
+                        <div className="col-md-3">
+                            <NewsItem title="News Today" description="Aaj ki taza khabar"/>
+                        </div>
+                        <div className="col-md-3">
+                            <NewsItem title="News Today" description="Aaj ki taza khabar"/>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
