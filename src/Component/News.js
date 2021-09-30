74a35600d712a814a0e66df1c103fca242208103
Sidharth Mishra <sidharth1604@gmail.com>
2021-09-30 23:13:46 +0530

    Next and Prev button Added

--- src/Component/News.js
+++ src/Component/News.js
@@ -5,17 +5,45 @@ export class News extends Component {
 
     constructor(){
         super();
-        //This is same as using UseState[text,setText] in function based component
+        //This is same as using const[text, setText] = useState(''); in function based component
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1
         }
     }
-    async componentDidMount(){      //Its runs after the render function
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=97b12b02d71a4d1d9ba7f5e2d82b36f1";
+    async componentDidMount(){   //Its runs after the render function
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=97b12b02d71a4d1d9ba7f5e2d82b36f1&page=1&pageSize=15";
         let data = await fetch(url)
         let parsedData = await data.json();
-        this.setState({articles: parsedData.articles})
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults
+        })
+    }
+
+    prevClick = async ()=>{
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=97b12b02d71a4d1d9ba7f5e2d82b36f1&page=${this.state.page-1}&pageSize=15`;
+        let data = await fetch(url)
+        let parsedData = await data.json();
+        this.setState({
+            articles: parsedData.articles,
+            page: this.state.page-1,
+        })
+    }
+    nextClick = async ()=>{
+        if(this.state.page+1 > Math.ceil(this.state.totalResults/15)){
+            
+        }
+        else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=97b12b02d71a4d1d9ba7f5e2d82b36f1&page=${this.state.page+1}&pageSize=15`;
+            let data = await fetch(url)
+            let parsedData = await data.json();
+            this.setState({
+                articles: parsedData.articles,
+                page: this.state.page+1,
+            })
+        }
     }
 
     render() {
@@ -29,6 +57,10 @@ export class News extends Component {
                             <NewsItem title={element.title ? element.title.slice(0,45) : " "} description={element.description ? element.description.slice(0,88) : " "} imgUrl={element.urlToImage} newsUrl={element.url}/>
                         </div>
                         })}
+                    </div>
+                    <div className="container mt-5 d-flex justify-content-lg-between">
+                        <button disabled={this.state.page<=1} type="button btn-lg" className="btn btn-dark" onClick={this.prevClick}>&larr; Previous</button>
+                        <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/15)} type="button btn-lg" className="btn btn-dark noNext" onClick={this.nextClick}>Next &rarr;</button>
                     </div>
                 </div>
             </>
