da506b3934d5083fdcaab0de800e7144ba7af3b8
Sidharth Mishra <sidharth1604@gmail.com>
2022-01-10 19:54:05 +0530

    Changes Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import "./App.css";
 import React, { Component } from 'react'
 import MediaNavbar from './Component/MediaNavbar'
 import News from './Component/News'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export class App extends Component {
   render() {
