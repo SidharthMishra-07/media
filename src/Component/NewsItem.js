d1cf8e82199a28e5872bf919a607679ee9af75c3
Sidharth Mishra <sidharth1604@gmail.com>
2022-01-10 19:54:21 +0530

    Changes added

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
         let {title,description,imgUrl,newsUrl} = this.props;
         return (
             <>
-                <div className="card" style={{width:"20rem", boxShadow:"5px 5px 10px #ced7ef"}}>
+                <div className="card" style={{boxShadow:"5px 5px 10px #ced7ef"}}>
                     <img src={!imgUrl ? "https://images.unsplash.com/photo-1585776245991-cf89dd7fc73a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bmV3c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" : imgUrl} className="card-img-top" alt="card_img"/>
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
